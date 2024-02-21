ba3debe87c6bd2b802223d9e79afe41b89359a86
Anthony Gavidia-Vasquez <98444525+Gavisito@users.noreply.github.com>
2024-02-20 21:17:16 -0500

    adj app variable

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import requests
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__, template_folder='templates')
-app = Flask(__name__)
 
 # Load API key from environment variable or configuration file
 import openai
